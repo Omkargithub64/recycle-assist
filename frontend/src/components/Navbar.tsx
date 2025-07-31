7f8fbef106de05b4ba794982e6ad932116cb6001
Omkargithub64 <omkarbhute26@gmail.com>
2025-07-31 19:25:36 +0530

    authentication done

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,38 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import './Navbar.css'
+import axios from "axios"
 
 function Navbar() {
+
+    const navigate = useNavigate()
+    axios.defaults.withCredentials = true
+
+    const handellogout = () => {
+        axios.get('http://localhost:8081/logout')
+            .then(
+                res => {
+                    if (res.data.Status === "Sucess") {
+                        navigate('/')
+                    } else {
+                        alert(res.data.Error)
+                    }
+                }
+            )
+    }
+
+
+
+
+
     return (
         <nav className="navbar">
             <div className="navbar-logo">
-                ♻️ RecycleAI
+                RecycleAI
             </div>
             <div className="navbar-links">
                 <NavLink to="/home" className="nav-link">Scan</NavLink>
                 <NavLink to="centers" className="nav-link">Centers</NavLink>
-                <NavLink to="/" className="nav-link">Log out</NavLink>
+                <button onClick={handellogout} className="nav-link">Log out</button>
                 <NavLink to="profile" className="nav-link">Profile</NavLink>
             </div>
         </nav>
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,32 +1,67 @@
 import { Link } from "react-router-dom";
 import './Home.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
+
+    const [auth, setauth] = useState(false);
+    const [message, setmessage] = useState('');
+    axios.defaults.withCredentials = true;
+
+    useEffect(() => {
+        axios.get('http://localhost:8081/verify')
+            .then(
+                res => {
+                    if (res.data.Status == "Sucess") {
+                        setauth(true)
+                    } else {
+                        setauth(false)
+                        setmessage(res.data.Error)
+
+                    }
+                }
+            )
+    }, [])
+
+
+
+
+
+
     return (
         <div className="scan-container">
-            <div className="scan-card">
-                <h1 className="scan-title">Scan Waste Item</h1>
+            {
+                auth ?
+                    <div className="scan-card">
+                        <h1 className="scan-title">Scan Waste Item</h1>
 
-                <div className="upload-section">
-                    <label htmlFor="image-upload" className="upload-label">
-                        Upload Image
-                    </label>
-                    <input
-                        id="image-upload"
-                        type="file"
-                        accept="image/*"
-                        className="upload-input"
-                    />
-                </div>
+                        <div className="upload-section">
+                            <label htmlFor="image-upload" className="upload-label">
+                                Upload Image
+                            </label>
+                            <input
+                                id="image-upload"
+                                type="file"
+                                accept="image/*"
+                                className="upload-input"
+                            />
+                        </div>
 
-                <div className="image-preview-box">
-                    {/* In logic: Show preview here if image is uploaded */}
-                    <p className="preview-placeholder">Image preview will appear here.</p>
-                </div>
-                <Link to="result">
-                <button className="scan-button">Scan Item</button>
-                </Link>
-            </div>
+                        <div className="image-preview-box">
+                            {/* In logic: Show preview here if image is uploaded */}
+                            <p className="preview-placeholder">Image preview will appear here.</p>
+                        </div>
+                        <Link to="result">
+                            <button className="scan-button">Scan Item</button>
+                        </Link>
+                    </div>
+                    :
+                    <div className="scan-card"><Link to="/">
+                        <h1 className="scan-title">{message}</h1>
+                        <button className="scan-button">Login</button>
+                    </Link></div>
+            }
         </div>
     )
 }
